f9b545fd22e9b741fbb8be829c7064ba71a0d99a
dl-alexandre <166029845+dl-alexandre@users.noreply.github.com>
2026-07-23 10:47:14 -0700

    fix(mobile): handle pairing deep links on iOS

--- native/devide_mob/test/devide_mob/ios_configuration_test.exs
+++ native/devide_mob/test/devide_mob/ios_configuration_test.exs
@@ -2,6 +2,7 @@ defmodule DevideMob.IOSConfigurationTest do
   use ExUnit.Case, async: true
 
   @info_plist Path.expand("../../ios/Info.plist", __DIR__)
+  @app_delegate Path.expand("../../ios/AppDelegate.m", __DIR__)
 
   test "uses the resizable iPad window model with a launch screen" do
     plist = File.read!(@info_plist)
@@ -18,5 +19,16 @@ defmodule DevideMob.IOSConfigurationTest do
         ] do
       assert plist =~ "<string>#{orientation}</string>"
     end
+  end
+
+  test "routes pair and review deep links through the native notification bridge" do
+    app_delegate = File.read!(@app_delegate)
+
+    assert app_delegate =~ "MobNotificationJSONFromReviewURL"
+    assert app_delegate =~ "MobNotificationJSONFromPairURL"
+    assert app_delegate =~ ~s(@"action": @"mobile.pair")
+    assert app_delegate =~ ~s(@"pairing_code": code)
+    assert app_delegate =~ "MobStoreDeepLinkURL(context.URL)"
+    assert app_delegate =~ "return MobStoreDeepLinkURL(url);"
   end
 end
